b7cc37ab3f82bf46feb5da4f659bcb76903d7eb6
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-18 23:11:56 +0900

    バリデーションの文言のインプット

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {useForm} from "react-hook-form";
+import React from "react";
 
 const App = () => {
 
@@ -17,11 +18,12 @@ const App = () => {
         <h1>ログインフォーム</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name">名前</label>
-            <input id="name" type="text" {...register("name")}/>
+            <input id="name" type="text" {...register("name", {required: "名前は必須です。"})}/>
+            <p>{errors.name?.message as React.ReactNode}</p>
             <label htmlFor="mail">メールアドレス</label>
-            <input id="mail" type="email" {...register("mail")}/>
+            <input id="mail" type="email" {...register("mail", {required: "メールアドレスは必須です。"})}/>
             <label htmlFor="password">パスワード</label>
-            <input id="password" type="password" {...register("password")}/>
+            <input id="password" type="password" {...register("password", {required: "パスワードは必須です。"})}/>
             <button type="submit">送信</button>
         </form>
     </div>
