39905fed44e254670f99082dd2fcabcb3f0356a6
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-18 22:35:30 +0900

    ログインフォームの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import './App.css'
 const App = () => {
 
   return (
-    <>Hello</>
+    <div className="form-container">
+        <form>
+            <label htmlFor="name">名前</label>
+            <input id="name" type="text" />
+            <label htmlFor="mail">メールアドレス</label>
+            <input id="mail" type="email" />
+            <label htmlFor="password">パスワード</label>
+            <input id="password" type="password" />
+            <button type="submit">送信</button>
+        </form>
+    </div>
   )
 }
 
