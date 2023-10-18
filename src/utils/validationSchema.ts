fad82535b6072f92e28db9e456a25c651aa6e865
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-18 23:39:25 +0900

    zodインストール＆バリデーションスキーマの設定

---
+++ src/utils/validationSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const validationSchema = z.object({
+    name: z.string().nonempty("名前は必須です").min(4, "4文字以上で入力してください"),
+    email: z.string().nonempty("メールアドレスは必須です").email("正しいメールアドレスを入力してください"),
+    password: z.string().nonempty("パスワードは必須です").min(4, "4文字以上で入力してください")
+})
