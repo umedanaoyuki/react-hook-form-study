6945242d9fca66a51aff02485e36c639f882254c
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-10-18 22:56:19 +0900

    状態管理register, handleSubmitの実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import {useForm} from "react-hook-form";
 
 const App = () => {
 
-    const { register, handleSubmit, formState: {errors}} = useForm();
+    const { register,
+        handleSubmit,
+        formState: { errors},
+    } = useForm();
 
     const onSubmit = (data: any) => {
         console.log(data);
     };
-    
+
   return (
     <div className="form-container">
         <h1>ログインフォーム</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label htmlFor="name">名前</label>
-            <input id="name" type="text" />
+            <input id="name" type="text" {...register("name")}/>
             <label htmlFor="mail">メールアドレス</label>
-            <input id="mail" type="email" />
+            <input id="mail" type="email" {...register("mail")}/>
             <label htmlFor="password">パスワード</label>
-            <input id="password" type="password" />
+            <input id="password" type="password" {...register("password")}/>
             <button type="submit">送信</button>
         </form>
     </div>
